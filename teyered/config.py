4a581247850f33328bbf56fd1d7569a3bb914954
Kacper Kielak <kielak.kacper@gmail.com>
2019-02-04 17:09:21 +0000

    #7 Speed up points extraction
    Speed up obtained by creating a points extraction class that keeps
    and reuses detection and prediction objects instead of creating
    then from scratch for every extraction.

--- teyered/config.py
+++ teyered/config.py
@@ -7,3 +7,8 @@ RESOURCES_DIR = os.path.join(PROJECT_ROOT_DIR, 'resources')
 
 PREDICTOR_FILENAME = 'shape_predictor_68_face_landmarks.dat'
 PREDICTOR_FILEPATH = os.path.join(RESOURCES_DIR, PREDICTOR_FILENAME)
+
+
+# Detection/prediction configuration
+IMAGE_UPSAMPLE_FACTOR = 1  # Ease facial landmark detection (value from dlib)
+UNIVERSAL_RESIZE = 500  # [px] Points are stored at this size
