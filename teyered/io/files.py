07ec11724e642e3f469f70f1bd8d7e04fb027443
Kacper Kielak <kielak.kacper@gmail.com>
2019-02-04 16:17:57 +0000

    #5 Create VideoGenerator
    VideoGenerator takes advantage of lazy evaluation. Instead of
    loading all frames at once, user can create a generator that opens
    a video and loads frames when needed.

--- teyered/io/files.py
+++ teyered/io/files.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import cv2
 
@@ -6,57 +7,88 @@ import cv2
 logger = logging.getLogger(__name__)
 
 
-def load_video(video_file_path, frames_to_skip):
-    """
-    Load existing video file into the tool
-    :param video_file_path: Full path to the video file
-    :param frames_to_skip: Every nth frame will be analyzed
-    :return: Video frames
-    """
-    frames = []
+class VideoGenerator:
 
-    cap = cv2.VideoCapture(video_file_path)
+    def __init__(self, video_path, frames_to_skip):
+        self._vid_path = video_path
+        self._frames_to_skip = frames_to_skip
+        self._vid = None
+        self._vid_fps = None
+        self._vid_frames_count = None
+        self._next_frame_data = None
 
-    # Video parameters
-    video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    video_fps = int(cap.get(cv2.CAP_PROP_FPS))
-    video_frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    def __enter__(self):
+        self.open()
+        return self
 
-    logger.info(f'\Loading video {video_file_path}:')
-    logger.info(f'Width (px): {video_width}')
-    logger.info(f'Height (px): {video_height}')
-    logger.info(f'FPS: {video_fps}')
-    logger.info(f'Frames count: {video_frames_count}')
-    logger.info(f'Analysing every {frames_to_skip}th frame')
+    def open(self):
+        logger.info(f'Loading video {self._vid_path}:')
+        if not os.path.isfile(self._vid_path):
+            raise AttributeError(f'File like {self._vid_path} does not exist.')
 
-    counter = -1  # Setting to -1 saves some hassle with lines
-    while cap.isOpened():
-        # Check if the video is over
-        if cap.get(1) == video_frames_count:
-            logger.info('Video is over')
-            break
+        self._vid = cv2.VideoCapture(self._vid_path)
+        self._vid_fps = int(self._vid.get(cv2.CAP_PROP_FPS))
+        self._vid_frames_count = int(self._vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        ret, frame = cap.read()
-        counter += 1
-        frame_timespan = counter / video_fps  # [s]
+        logger.info(f'Width (px): {self._vid.get(cv2.CAP_PROP_FRAME_WIDTH)}')
+        logger.info(f'Height (px): {self._vid.get(cv2.CAP_PROP_FRAME_HEIGHT)}')
+        logger.info(f'FPS: {self._vid_fps}')
+        logger.info(f'Frames count: {self._vid_frames_count}')
+        logger.info(f'Analysing every {self._frames_to_skip}th frame')
+        self._read_next_frame()
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        if not self.is_open():
+            raise IOError('Cannot close the video, '
+                          'it is not open in the first place.')
+        logger.info(f'\Closing video {self._vid_path}:')
+        self._vid.release()
+
+    def get_next_frame(self):
+        if not self.is_open():
+            raise IOError('Cannot process the video, file is not open.')
+
+        if self.is_over():
+            raise IOError('Video is over, cannot read next frame.')
+
+        frame_timespan, frame = self._next_frame_data
+        self._read_next_frame()
+        return frame_timespan, frame
+
+    def _read_next_frame(self):
+        # Skip frames
+        frame_num = self._vid.get(cv2.CAP_PROP_POS_FRAMES)
+        while not self.is_over() and frame_num % self._frames_to_skip != 0:
+            self._vid.read()
+            frame_num = self._vid.get(cv2.CAP_PROP_POS_FRAMES)
+
+        if self.is_over():
+            logger.info('Video finished')
+            return
+
+        # Read frame to memory
+        ret, frame = self._vid.read()
         if not ret:
-            logger.warning(f'Frame {counter} could not be read')
-            continue
+            logger.warning(f'Frame {frame_num} could not be read.')
+            self._read_next_frame()
+            return
 
-        # Showing only every n-th frame
-        if (counter % frames_to_skip) != 0:
-            continue
+        logger.debug(f'Loading frame no:. {frame_num}')
+        frame_timespan = frame_num / self._vid_fps  # [s]
+        self._next_frame_data = (frame_timespan, frame)
 
-        frames.append((frame_timespan, frame))
+    def is_open(self):
+        return self._vid is not None and self._vid.isOpened()
 
-    logger.info(f'Video loading for {video_file_path} has finished')
+    def is_over(self):
+        if not self.is_open():
+            raise IOError('Cannot process the video, file is not open.')
 
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return frames
+        curr_frame_num = self._vid.get(cv2.CAP_PROP_POS_FRAMES)
+        return curr_frame_num == self._vid_frames_count
 
 
 def load_image(image_file_path):
