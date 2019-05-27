Fix bugs and add reprojection error calculation

teyered/config.py
@@ -15,8 +15,8 @@ FACE_MODEL_FILEPATH = os.path.join(RESOURCES_DIR, FACE_MODEL_FILENAME)
 
 # Image processing configuration
 IMAGE_UPSAMPLE_FACTOR = 1  # Ease facial landmark detection (value from dlib)
-UNIVERSAL_RESIZE = 500  # [px] Photo processing size
-ASPECT_RATIO = 333/500
+UNIVERSAL_RESIZE = 1080 # [px] Photo processing size
+ASPECT_RATIO = 720/1080
 
 # Lower and upper boundaries of the facial feature labels
 JAW_COORDINATES = (0, 17)
@@ -27,6 +27,15 @@ RIGHT_EYE_COORDINATES = (36, 42)
 LEFT_EYE_COORDINATES = (42, 48)
 MOUTH_COORDINATES = (48, 68)
 
+# Need to know aspect ratio
+CAMERA_MATRIX = np.array(
+                             [[962.51477715, 0, 509.06946124],
+                             [0, 961.84061336, 337.23457898],
+                             [0, 0, 1]], dtype = "double"
+                         )
+DIST_COEFFS = np.array([[-0.0992409, 1.0407034, -0.00665748, -0.01156595, -2.11200394]], dtype="double")
+
+"""
 # Approximation for camera parameters
 FOCAL_LENGTH = UNIVERSAL_RESIZE
 CENTER = (UNIVERSAL_RESIZE / 2, UNIVERSAL_RESIZE*ASPECT_RATIO / 2)  # Need to know aspect ratio
@@ -36,3 +45,4 @@ CAMERA_MATRIX = np.array(
                              [0, 0, 1]], dtype = "double"
                          )
 DIST_COEFFS = np.zeros((4, 1))
+"""

teyered/data_processing/eye_normalization.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 from teyered.config import CAMERA_MATRIX, DIST_COEFFS
-
+from teyered.head_pose.pose import choose_pose_points
 
 logger = logging.getLogger(__name__)
 
@@ -37,8 +37,10 @@ def project_eye_points(frames, facial_points_all, model_points, r_vectors_all, t
 
     frame_count = 0
     for frame in frames:
-        if facial_points_all[frame_count] is None:
+        if facial_points_all[frame_count] == []:
             model_points_projected_all.append([])
+            frame_count += 1
+            continue
 
         model_points_projected, _ = cv2.projectPoints(model_points, 
             r_vectors_all[frame_count], t_vectors_all[frame_count], CAMERA_MATRIX, 
@@ -48,6 +50,39 @@ def project_eye_points(frames, facial_points_all, model_points, r_vectors_all, t
         frame_count += 1
 
     return np.array(model_points_projected_all)
+
+def calculater_reprojection_error_live(facial_points, reprojected_points):
+    """
+    Calculate reprojection error based on euclidian distance between points
+    :param facial_points: Facial points used in pose estimation
+    :param reprojected_points: Reprojected 3D model points used in pose estimation
+    :return: Float value of the overall error
+    """
+    return np.sum(np.power(np.sum(np.power(facial_points-reprojected_points,2), axis=1), 0.5))
+
+def calculater_reprojection_error(frames, facial_points_all, reprojected_points_all):
+    """
+    Calculate reprojection error based on euclidian distance between points. Difference from live version is that the pose points are chosen here instead of being already passed as a parameter
+    :param frames: All frames
+    :param facial_points_all: All facial points in all frames
+    :param reprojected_points_all: All reprojected points from 3D model in all frames
+    :return: A list of float values of the overall error (ready to be saved in a .csv file using file.py save_points method)
+    """
+    errors = []
+
+    frame_count = 0
+    for frame in frames:
+        if facial_points_all[frame_count] == []:
+            errors.append((frame_count,0))
+            frame_count += 1
+            continue
+
+        error = np.sum(np.power(np.sum(np.power(choose_pose_points(facial_points_all[frame_count]) - choose_pose_points(reprojected_points_all[frame_count]),2), axis=1), 0.5))
+
+        errors.append((frame_count, error))
+        frame_count += 1
+
+    return errors
 
 def compare_projected_facial(model_points_projected, facial_points):
     """

teyered/io/image_processing.py
@@ -39,6 +39,10 @@ def draw_pose(frames, facial_points_all, r_vectors_all, t_vectors_all,
     for i in range(0, len(frames)):
         edited_frame = frames[i]
 
+        if facial_points_all[i] == []:
+            edited_frames.append(edited_frame)
+            continue
+
         rot_x, _ = cv2.projectPoints(np.array([(1.0, 0.0, 0.0)]),
                                      r_vectors_all[i], t_vectors_all[i],
                                      camera_matrix, dist_coeffs)
@@ -78,6 +82,11 @@ def draw_facial_points(frames, facial_points_all):
 
     for i in range(0, len(frames)):
         edited_frame = frames[i]
+
+        if facial_points_all[i] == []:
+            edited_frames.append(edited_frame)
+            continue
+
         for point in facial_points_all[i]:
             cv2.circle(edited_frame, (int(point[0]), int(point[1])),
                        1, (0, 255, 0), -1)
@@ -100,6 +109,11 @@ def draw_projected_points(frames, projected_points):
 
     for i in range(0, len(frames)):
         edited_frame = frames[i]
+
+        if projected_points[i] == []:
+            edited_frames.append(edited_frame)
+            continue
+
         for point in projected_points[i]:
             cv2.circle(edited_frame, (int(point[0][0]), int(point[0][1])), 1, (0, 0, 255), -1)
         edited_frames.append(edited_frame)
@@ -113,7 +127,12 @@ def write_angles(frames, angles_all, color):
     for i, frame in enumerate(frames):
         edited_frame = frame
 
-        angles_all = np.around(angles_all, decimals=1)
+        if angles_all[i] == []:
+            edited_frames.append(edited_frame)
+            continue
+
+        print(angles_all[i])
+        angles_all[i] = np.around(angles_all[i], decimals=1)
 
         angle_text_yaw = f'yaw: {angles_all[i][0][0]}'
         angle_text_pitch = f'pitch: {angles_all[i][1][0]}'

teyered/ui/display_3D.py
@@ -17,11 +17,11 @@ import math
 from teyered.config import CAMERA_MATRIX, DIST_COEFFS, UNIVERSAL_RESIZE
 from teyered.head_pose.camera_model import CameraModel
 from teyered.io.image_processing import draw_pose_frame, draw_facial_points_frame, gray_image, resize_image, write_angles_frame
-from teyered.head_pose.pose import estimate_pose_live, _prepare_face_model, _get_rotation_matrix, _prepare_original_face_model, _choose_pose_points
+from teyered.head_pose.pose import estimate_pose_live, get_rotation_matrix
 from teyered.data_processing.points_extractor import FacialPointsExtractor
 from teyered.io.files import load_video, load_image
 
-from teyered.head_pose.face_model_processing import optimize_face_model, get_ground_truth
+from teyered.head_pose.face_model_processing import optimize_face_model, get_ground_truth, load_face_model
 
 import time
 
@@ -34,6 +34,10 @@ cap = cv2.VideoCapture(0)
 
 GROUND_TRUTH_FRAME = 'ground_truth/frame.jpg'
 
+RED_COLOR = (1.0, 0.0, 0.0, 1.0)
+GREEN_COLOR = (0.0, 1.0, 0.0, 1.0)
+BLUE_COLOR = (0.0, 0.0, 1.0, 1.0)
+YELLOW_COLOR = (1.0, 1.0, 0.0, 1.0)
 
 class Live3D():
 
@@ -187,28 +191,26 @@ class Live3D():
         self.frame_count = 0
         self.cap = cv2.VideoCapture(0)
 
-        o_model_points_all = _prepare_original_face_model()
-        ground_truth = get_ground_truth(load_image(GROUND_TRUTH_FRAME), self.points_extractor)
-        (model_points_all, facial_points_z, model_points_z) = optimize_face_model(ground_truth, o_model_points_all)
+        # Setup model points
+        model_points_original = load_face_model()
+        facial_points_ground_truth = get_ground_truth(load_image(GROUND_TRUTH_FRAME), self.points_extractor)
+        (model_points_optimized, facial_points_norm, model_points_norm) = optimize_face_model(facial_points_ground_truth, model_points_original)
+        self.model_points = model_points_optimized # Set model points here
 
-        self.model_points = _choose_pose_points(model_points_all)
+        print(model_points_optimized[37])
 
-        """
         self.model_points_scatter.setData(
-            pos = model_points_all*2,
-            color = (0.5, 0.0, 0.0, 1.0), #R
+            pos = model_points_optimized*2,
+            color = RED_COLOR,
             size = 10,
             pxMode = True
         )
-        """
-        """
         self.o_model_points_scatter.setData(
-            pos = model_points_z*2,
-            color = (0.0, 0.5, 0.0, 1.0), #G
+            pos = model_points_norm*2,
+            color = GREEN_COLOR,
             size = 10,
             pxMode = True
         )
-        """
         """
         self.to_model_points_scatter.setData(
             pos = o_model_points_all*0.025,
@@ -219,12 +221,12 @@ class Live3D():
         """
         """
         self.facial_points_scatter.setData(
-            pos = facial_points_z*2,
-            color = (0.5, 0.0, 0.0, 1.0), #BG
+            pos = facial_points_norm*2,
+            color = BLUE_COLOR,
             size = 10,
             pxMode = True
         )
-        """ 
+        """
         """
         old_model = np.array([[0.0,0.0,0.0],[0.0,-330.0, -65.0],[-225.0,170.0,-135.0], [225.0,170.0,-135.0],[-150.0,-150.0,-125.0],[150.0,-150.0,-125.0]])
         self.old_face_model_scatter.setData(