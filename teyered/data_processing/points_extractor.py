ca236d8b5d034791f62bf92ced11e26205a1a01f
Karolishp <karolis.spukas@gmail.com>
2019-07-23 19:25:04 +0100

    Fix repository according to PR and call 4

--- teyered/data_processing/points_extractor.py
+++ teyered/data_processing/points_extractor.py
@@ -2,10 +2,11 @@ import logging
 
 import cv2
 import dlib
-from imutils import face_utils, resize
 import numpy as np
+from imutils import face_utils
 
-from teyered.config import IMAGE_UPSAMPLE_FACTOR, PREDICTOR_FILEPATH, TRACKING_LENGTH
+from teyered.config import IMAGE_UPSAMPLE_FACTOR, PREDICTOR_FILEPATH, \
+    TRACKING_LENGTH
 
 
 logger = logging.getLogger(__name__)
@@ -19,98 +20,123 @@ class FacialPointsExtractor:
         self._predictor = dlib.shape_predictor(PREDICTOR_FILEPATH)
 
         # LK optical flow tracking parameters
-        self._LK_PARAMS = dict(winSize  = (15,15),
-                               maxLevel = 3,
-                               criteria = (cv2.TERM_CRITERIA_EPS | \
-                                           cv2.TERM_CRITERIA_COUNT,
-                                           10, 0.03))
+        self._LK_PARAMS = dict(winSize=(15, 15), maxLevel=3,
+                               criteria=(cv2.TERM_CRITERIA_EPS |
+                                         cv2.TERM_CRITERIA_COUNT,
+                                         10, 0.03)
+                               )
 
-    def track_facial_points_LK(self, previous_frame, new_frame,
-                                previous_points, detected_points):
+        # Previous batch information
+        self._previous_frame = None
+        self._previous_points = None
+        self._frame_count = 0
+
+    def reset(self):
+        self._previous_frame = None
+        self._previous_points = None
+        self._frame_count = 0
+
+    def get_previous_frame(self):
+        return self._previous_frame
+
+    def get_previous_points(self):
+        return self._previous_points
+
+    def get_frame_count(self):
+        return self._frame_count
+
+    def _track_facial_points_LK(self, new_frame, detected_points):
         """
         Track facial points using Lucas-Kanade optical flow
-        :param previous_frame: Previous frame numpy array in grayscale scaled
-        :param new_frame: Current frame numpy array in grayscale scaled
-        :param previous_points: Feature points in previous frame
-        :param detected_points: Detected points in the new frame for relative
+        TODO Redetect individual points based on their errors
+        TODO Try other tracking methods
+        :param new_frame: Current frame in grayscale scaled
+        :param detected_points: Detected points in new frame for relative
         error estimation
         :return: np.ndarray of tracked points or None if no points are tracked
         """
         # Get the estimate of the new points
         new_points, status, _ = cv2.calcOpticalFlowPyrLK(
-             previous_frame, new_frame, previous_points.astype(np.float32),
-             None, **self._LK_PARAMS)
+            self._previous_frame, new_frame,
+            self._previous_points.astype(np.float32), None, **self._LK_PARAMS
+        )
 
         # Get the points which were tracked
         points_status = np.hstack((new_points, status))
-        new_points_good = np.array(points_status[points_status[:,2] == 1],
+        new_points_good = np.array(points_status[points_status[:, 2] == 1],
                                    dtype=np.float32)
 
         # If some points were not tracked, redetect all
         if new_points_good.shape[0] != detected_points.shape[0]:
             return None
 
-        return new_points_good[:,0:2]
+        return new_points_good[:, 0:2]
 
-    def detect_facial_points(self, frame):
+    def _detect_facial_points(self, frame):
         """
         Detect facial points of the first detected face in a frame using dlib
+        TODO Change the detection to our CNN
+        TODO Deal with multiple faces being discovered
         :param frame: Frame grayscale scaled
-        :return: np.ndarray of detected facial features in the frame scaled to frame size or None if not detected
+        :return: np.ndarray of detected facial features in the frame scaled
+        to frame size or None if not detected
         """
-        for (i, rect) in enumerate(self._detector(frame, IMAGE_UPSAMPLE_FACTOR)):
-            facial_points = self._predictor(frame, rect)
+        # Rectangles of multiple faces
+        rect = self._detector(frame, IMAGE_UPSAMPLE_FACTOR)
+
+        # Only use the first face image
+        if len(rect) > 0:
+            facial_points = self._predictor(frame, rect[0])
             facial_points = face_utils.shape_to_np(facial_points)
             return facial_points
+        else:
+            return None
 
-    def extract_facial_points(self, frames, previous_frame = None, previous_points = None, frame_count = 0):
+    def extract_facial_points(self, frames):
         """
-        Extract facial points from a video sequence using detection and tracking. Accounts for calling it in the middle of the tracking, but previous points, 
-        previous frame and frame count must be provided together
+        Extract facial points from a video sequence combining detection and
+        tracking
+        TODO Introduce filtering (like Kalman)
         :param frames: All frames to be analysed
-        :param previous_frame: In case the method is called in the middle of the frame, pass previous detected/tracked points
-        :param previous_points: In case the method is called in the middle of the frame, pass a previous frame
-        :param frame_count: In case the method is called in the middle of the frame, tracking may be done instead of new detection
-        :return: Array of all facial points in each frame and the current frame count (in case the method is called on portions of video)
+        :return: np.ndarray of all facial points in each frame
         """
         if frames.shape[0] < 1:
             raise ValueError('At least one frame is required')
-        if (previous_frame is None and previous_points is not None) or (previous_frame is not None and previous_points is None):
-            raise ValueError('Previous frame and previous points must be provided together')
-        if previous_frame is None and not frame_count == 0:
-            raise ValueError('If previous frame and previous points are not provided, frame count must be 0')
-        
+
+        # Information to be returned
         facial_points_all = []
 
         for frame in frames:
             # Detect facial points as this will be needed in any case
-            detected_facial_points = self.detect_facial_points(frame)
+            detected_facial_points = self._detect_facial_points(frame)
 
-            # No facial points detected, skip this frame and detect in the next iteration
+            # No facial points detected, skip this frame and detect in the
+            # next iteration
             if detected_facial_points is None:
                 facial_points_all.append(None)
-                frame_count = 0
+                self._frame_count = 0
                 continue
 
             # (Re)detect at every TRACKING_LENGTHth frame
-            if frame_count % TRACKING_LENGTH == 0:
-                previous_points = detected_facial_points
-                previous_frame = frame
-                frame_count = 1
+            if self._frame_count % TRACKING_LENGTH == 0:
+                self._previous_points = detected_facial_points
+                self._previous_frame = frame
+                self._frame_count = 1
             # Track
             else:
-                previous_points = self.track_facial_points_LK(
-                    previous_frame, frame, previous_points,
-                    detected_facial_points)
-                previous_frame = frame
-                frame_count += 1
-                
+                self._previous_points = self._track_facial_points_LK(
+                    frame, detected_facial_points
+                )
+                self._previous_frame = frame
+                self._frame_count += 1
+
                 # Tracking is unsuccessful, redetect
-                if previous_points is None:
-                    previous_points = detected_facial_points
-                    frame_count = 1
+                if self._previous_points is None:
+                    self._previous_points = detected_facial_points
+                    self._frame_count = 1
 
-            facial_points_all.append(previous_points)
+            facial_points_all.append(np.copy(self._previous_points))
 
-        logger.debug('Facial points were successfully extracted from the image')
-        return (np.array(facial_points_all), frame_count)
+        logger.debug(
+            'Facial points were successfully extracted from the video batch')
+        return np.array(facial_points_all)
